d74d60025c435b0b992cae18976f62217c6f651d
tt2819ais-arch <tt2819ais@gmail.com>
2026-01-16 15:50:00 +0300

    Delete server directory

--- server/src/repositories/base.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC
-from dataclasses import dataclass
-
-from src.repositories.database.base import DatabaseRepositories
-
-
-@dataclass
-class Repositories(ABC):
-    database: DatabaseRepositories | None = None
